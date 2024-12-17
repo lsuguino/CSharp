ed59050fbc7672f3295884035864c1ecd960d765
Leonardo Suguino <leonardo_suguino@hotmail.com>
2024-12-16 22:04:33 -0300

    Create Media.cs

---
+++ Media.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double nota1, nota2, media;
+
+        nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        media = (nota1 + nota2) / 2;
+
+        Console.WriteLine("A sua media final é " + media.ToString("f1") + " Parabéns!");
+
+        if (media < 60.00)
+        {
+            Console.WriteLine("REPROVADO!");
+        }
+    }
+}
