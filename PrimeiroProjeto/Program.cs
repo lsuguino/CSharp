320b0c60ae023e4dfae4a96bccb31699174ced6c
leona <leona@SUGUINO>
2024-12-16 21:11:47 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int tabuada, contador, resultado;
+
+        Console.WriteLine("Qual tabuada você quer que seja calculada?");
+        tabuada = int.Parse(Console.ReadLine());
+        for (contador = 1; contador <= 10; ++contador) 
+        {
+            resultado = tabuada * contador;
+            Console.WriteLine("X = " + resultado);
+        }
+    }
+}
+
+
+
