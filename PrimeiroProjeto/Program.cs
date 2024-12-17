5bc1aa61ddbe3a39865d5433361c00e4990c8a0c
Leonardo Suguino <167928232+lsuguino@users.noreply.github.com>
2024-12-17 11:53:17 -0300

    Add files via upload
    Esse é um código em C# que tem como objetivo calcular uma equação de segundo grau, utilizando a fórmula de Bhaskara.
    
    This is a code in C# to calculate an second degree equation, using the Bhaskara formula.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bhaskara
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, x1, x2;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            if (a == 0 || delta == 0)
+            {
+                Console.WriteLine("Impossível de ser calculado, o valor de A e Delta não podem ser ZERO");
+            }
+            else
+            {
+                x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+                Console.WriteLine("O valor de x1 é " + x1.ToString("F5"), CultureInfo.InvariantCulture);
+                Console.WriteLine("O valor de x2 é " + x2.ToString("F5"), CultureInfo.InvariantCulture);
+            }
+            Console.ReadLine();
+        }
+    }
+}
